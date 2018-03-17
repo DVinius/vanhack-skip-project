dca99dc557b529cd60b11dc820601faf9a2862cd
ascaloners@gmail.com <ascaloners@gmail.com>
2018-03-17 18:15:31 -0300

    Jenkins setup

--- src/main/java/com/vsgdev/app/StartApp.java
+++ src/main/java/com/vsgdev/app/StartApp.java
@@ -1,5 +1,13 @@
 package com.vsgdev.app;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class StartApp {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(StartApp.class, args);
+	}
 
 }
