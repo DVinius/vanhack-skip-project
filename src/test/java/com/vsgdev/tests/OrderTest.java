e2e4da7d699032506df39b7fba34940bbc44b15a
ascaloners@gmail.com <ascaloners@gmail.com>
2018-03-18 11:59:23 -0300

    Test classes

---
+++ src/test/java/com/vsgdev/tests/OrderTest.java
@@ -0,0 +1,14 @@
+package com.vsgdev.tests;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class OrderTest {
+	
+	@Test
+	public void shouldRunWithError() {
+		assertTrue(false);
+	}
+
+}
